a30e02ee806913e95be712dc11623c51c167a41d
Cel Pynenborg <cel.pynenborg@telenetgroup.be>
2025-05-05 12:47:16 +0200

    Create Rollout whose traffic is routed via ALB instead of kube-proxy and create only one ALB

--- apply-all.sh
+++ apply-all.sh
@@ -1,4 +1,4 @@
-FOLDERS="01-recreate 02-rolling 03-vanilla-blue-green 04-basic-rollout"
+FOLDERS="01-recreate 02-rolling 03-vanilla-blue-green 04-basic-rollout 05-alb-rollout"
 
 for dest in $FOLDERS; do
   echo "Applying $dest"
