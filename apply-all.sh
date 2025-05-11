b9d5b4596bb5a2ab973a0b8732c6f397601e3195
Cel Pynenborg <cel.pynenborg@telenetgroup.be>
2025-05-11 07:30:00 +0200

    Final commit before haxx

--- apply-all.sh
+++ apply-all.sh
@@ -1,4 +1,4 @@
-FOLDERS="01-recreate 02-rolling 03-vanilla-blue-green 04-basic-rollout 05-alb-rollout"
+FOLDERS="01-recreate 02-rolling 03-vanilla-blue-green 04-basic-rollout 05-alb-rollout 06-analyzed-rollout"
 
 for dest in $FOLDERS; do
   echo "Applying $dest"
